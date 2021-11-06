0ae3acad707e628d75233b3789e4ac669316911b
John <johnab4444@gmail.com>
2021-11-05 21:03:12 -0400

    created group endpoints

---
+++ server/models/Group.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose"
+
+const GroupSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String, 
+        required: true,
+    },
+    follower_count: {
+        type: Number,
+        default: 0,
+    }
+})
+
+const Group = mongoose.model("Group", GroupSchema)
+export default Group
