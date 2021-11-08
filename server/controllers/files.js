1fb72c1cd96e7d47dc85c0c87e065bf1aa9fab3a
John <johnab4444@gmail.com>
2021-11-07 22:55:33 -0500

    added helper class for upload

--- server/controllers/files.js
+++ server/controllers/files.js
@@ -1,18 +1,4 @@
-import AWS from 'aws-sdk'
-import multer from 'multer'
-import multerS3 from 'multer-s3'
-import dotenv from 'dotenv'
-import { v4 as uuidv4 } from 'uuid'
-
-dotenv.config()
-
-AWS.config.update({
-  region: process.env.BUCKET_REGION,
-  secretAccessKey: process.env.BUCKET_SECRET_KEY,
-  accessKeyId: process.env.BUCKET_ACCESS_KEY,
-})
-
-var s3 = new AWS.S3()
+import { upload } from '../file-upload.js'
 
 /* *
  * * This function will upload all files to S3 bucket
@@ -114,17 +100,3 @@ export const delete_file = async (req, res) => {
         })
     }
 }
-
-const upload = multer({
-    storage: multerS3({
-        s3: s3,
-        bucket: process.env.BUCKET_NAME,
-        contentType: multerS3.AUTO_CONTENT_TYPE,
-        metadata: (req, file, cb) => {
-            cb(null, { fieldName: file.fieldname })
-        },
-        key: (req, file, cb) => {
-            cb(null, uuidv4())
-        },
-    }),
-}).array('files', 12)
